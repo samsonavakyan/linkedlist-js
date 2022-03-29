c6db6ff239036457bf7f59b8b866cfd315440c26
Samson <samson.avakyan@chulakov.ru>
2022-03-29 16:26:26 +0300

    Added implementation of the append method

---
+++ linkedlist.js
@@ -0,0 +1,51 @@
+class LinkedListNode {
+  constructor(value, next = null) {
+    this.value = value;
+  }
+
+  toString() {
+    return `${this.value}`;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    if (!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  toString() {
+    return this.toArray()
+      .map((node) => node.toString())
+      .toString();
+  }
+}
