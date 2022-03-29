4f231f4e009994c11f3c9a444b60c9b2e583206b
Samson <samson.avakyan@chulakov.ru>
2022-03-29 19:02:06 +0300

    Added tests for the delete method

--- linked-list.spec.js
+++ linked-list.spec.js
@@ -39,4 +39,29 @@ describe('Linked list', () => {
     expect(list.find('d').toString()).toBe('d');
     expect(list.find('x')).toBe(null);
   });
+
+  test('Delete', () => {
+    let list = init();
+
+    expect(list.delete('b').toString()).toBe('b');
+    expect(list.toString()).toBe('a,c,d');
+    list.delete('a');
+    expect(list.toString()).toBe('c,d');
+    expect(list.head.value).toBe('c');
+    list.delete('d');
+    expect(list.toString()).toBe('c');
+    expect(list.head.value).toBe('c');
+    expect(list.tail.value).toBe('c');
+  });
+
+  test('Delete multiple', () => {
+    let list = init();
+
+    list.append('a');
+
+    expect(list.delete('a').toString()).toBe('a');
+    expect(list.toString()).toBe('b,c,d');
+    expect(list.head.value).toBe('b');
+    expect(list.tail.value).toBe('d');
+  });
 });
