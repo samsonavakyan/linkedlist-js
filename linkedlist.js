30b61d51365250b3d59c040fad5e2965d3c0de91
Samson Avakyan <avakyansamsonsetraki@gmail.com>
2022-03-31 22:33:26 +0300

    Added implementation of the delete method

--- linkedlist.js
+++ linkedlist.js
@@ -61,6 +61,38 @@ class LinkedList {
     return null;
   }
 
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = null;
+
+    while (this.head && this.head.value === value) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+
+    if (currentNode !== null) {
+      while (currentNode.next) {
+        if (currentNode.next.value === value) {
+          deletedNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+
+    if (this.tail?.value === value) {
+      this.tail = currentNode;
+    }
+
+    return deletedNode;
+  }
+
   toArray() {
     const nodes = [];
 
